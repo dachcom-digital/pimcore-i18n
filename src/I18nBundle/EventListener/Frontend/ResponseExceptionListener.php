9472e1ceb73f54f82cc260f53b94019064570746
Stefan Hagspiel <shagspiel@dachcom.ch>
2018-04-17 17:47:04 +0200

    fix ReponesExceptionListener

--- src/I18nBundle/EventListener/Frontend/ResponseExceptionListener.php
+++ src/I18nBundle/EventListener/Frontend/ResponseExceptionListener.php
@@ -9,12 +9,12 @@ use I18nBundle\Manager\ZoneManager;
 use Pimcore\Cache;
 use Pimcore\Config;
 use Pimcore\Http\Exception\ResponseException;
+use Pimcore\Http\Request\Resolver\DocumentResolver;
+use Pimcore\Http\Request\Resolver\SiteResolver;
 use Pimcore\Model\Document;
-use Pimcore\Model\Site;
 use Pimcore\Http\Request\Resolver\PimcoreContextResolver;
 use Pimcore\Templating\Renderer\ActionRenderer;
 use Pimcore\Bundle\CoreBundle\EventListener\Traits\PimcoreContextAwareTrait;
-
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
@@ -46,6 +46,16 @@ class ResponseExceptionListener implements EventSubscriberInterface
     protected $pathGeneratorManager;
 
     /**
+     * @var SiteResolver
+     */
+    protected $siteResolver;
+
+    /**
+     * @var Document\Service
+     */
+    protected $documentService;
+
+    /**
      * @var bool
      */
     protected $renderErrorPage = true;
@@ -55,6 +65,8 @@ class ResponseExceptionListener implements EventSubscriberInterface
      * @param ZoneManager          $zoneManager
      * @param ContextManager       $contextManager
      * @param PathGeneratorManager $pathGeneratorManager
+     * @param SiteResolver         $siteResolver
+     * @param Document\Service     $documentService
      * @param bool                 $renderErrorPage
      */
     public function __construct(
@@ -62,12 +74,16 @@ class ResponseExceptionListener implements EventSubscriberInterface
         ZoneManager $zoneManager,
         ContextManager $contextManager,
         PathGeneratorManager $pathGeneratorManager,
+        SiteResolver $siteResolver,
+        Document\Service $documentService,
         $renderErrorPage = true
     ) {
         $this->actionRenderer = $actionRenderer;
         $this->zoneManager = $zoneManager;
         $this->contextManager = $contextManager;
         $this->pathGeneratorManager = $pathGeneratorManager;
+        $this->siteResolver = $siteResolver;
+        $this->documentService = $documentService;
         $this->renderErrorPage = (bool)$renderErrorPage;
     }
 
@@ -83,6 +99,7 @@ class ResponseExceptionListener implements EventSubscriberInterface
 
     /**
      * @param GetResponseForExceptionEvent $event
+     * @throws \Exception
      */
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
@@ -102,6 +119,7 @@ class ResponseExceptionListener implements EventSubscriberInterface
 
     /**
      * @param GetResponseForExceptionEvent $event
+     * @throws \Exception
      */
     protected function handleErrorPage(GetResponseForExceptionEvent $event)
     {
@@ -109,9 +127,99 @@ class ResponseExceptionListener implements EventSubscriberInterface
             return;
         }
 
-        //re-init zones since we're in a kernelException.
+        // re-init zones since we're in a kernelException.
         $zoneDomains = $this->zoneManager->getCurrentZoneDomains(true);
         $exception = $event->getException();
+
+        $host = $event->getRequest()->getHost();
+
+        // 1. get default system error page ($defaultErrorPath)
+        $defaultErrorPath = Config::getSystemConfig()->documents->error_pages->default;
+        $defaultErrorDocument = null;
+        $localizedErrorDocument = null;
+
+        if ($this->siteResolver->isSiteRequest($event->getRequest())) {
+            $path = $this->siteResolver->getSitePath($event->getRequest());
+            // 2. get site error page
+            $siteErrorPath = $this->siteResolver->getSite()->getErrorDocument();
+            if (!empty($siteErrorPath)) {
+                $defaultErrorPath = $siteErrorPath;
+            }
+        } else {
+            $path = urldecode($event->getRequest()->getPathInfo());
+        }
+
+        if (!empty($defaultErrorPath)) {
+            $defaultErrorDocument = Document::getByPath($defaultErrorPath);
+        }
+
+        $nearestDocumentLocale = null;
+        $nearestDocument = $this->documentService->getNearestDocumentByPath($path, true, ['page', 'hardlink']);
+
+        // 3. find localized error from current path
+        if ($nearestDocument instanceof Document) {
+
+            $nearestDocumentLocale = $nearestDocument->getProperty('language');
+
+            $validElements = array_keys(array_filter($zoneDomains,
+                function ($v) use ($host, $nearestDocumentLocale) {
+                    return $v['realHost'] === $host && $v['locale'] === $nearestDocumentLocale;
+                }));
+
+            //if we have a default error page, try to use same name.
+            $guessedErrorPath = 'error';
+            if ($defaultErrorDocument instanceof Document) {
+                $guessedErrorPath = $defaultErrorDocument->getKey();
+            }
+
+            if (!empty($validElements)) {
+                $validElement = $validElements[0];
+                $localizedPath = $zoneDomains[$validElement]['fullPath'] . DIRECTORY_SEPARATOR . $guessedErrorPath;
+                if (Document\Service::pathExists($localizedPath)) {
+                    $localizedErrorDocument = Document::getByPath($localizedPath);
+                }
+            }
+
+            // 4. find localized error page from source if path is in hard link context
+            if (!$localizedErrorDocument instanceof Document) {
+                if ($nearestDocument instanceof Document\Hardlink) {
+                    $nearestSourceDocument = $nearestDocument->getSourceDocument();
+                    $nearestSourceDocumentLocale = $nearestSourceDocument->getProperty('language');
+
+                    $validSourceElements = array_keys(array_filter($zoneDomains,
+                        function ($v) use ($host, $nearestSourceDocumentLocale) {
+                            return $v['realHost'] === $host && $v['locale'] === $nearestSourceDocumentLocale;
+                        }));
+
+                    if (!empty($validSourceElements)) {
+                        $validSourceElement = $validSourceElements[0];
+                        $localizedSourcePath = $zoneDomains[$validSourceElement]['fullPath'] . DIRECTORY_SEPARATOR . $guessedErrorPath;
+                        if (Document\Service::pathExists($localizedSourcePath)) {
+                            $localizedErrorDocument = Document::getByPath($localizedSourcePath);
+                        }
+                    }
+                }
+            }
+        }
+
+        $document = null;
+        if ($localizedErrorDocument instanceof Document) {
+            $document = $localizedErrorDocument;
+        } elseif ($defaultErrorDocument instanceof Document) {
+            $document = $defaultErrorDocument;
+        } else {
+            $document = Document::getById(1);
+        }
+
+        $this->setRuntime($document->getProperty('language'));
+        $this->contextManager->getContext()->setDocument($document);
+
+        try {
+            $response = $this->actionRenderer->render($document);
+        } catch (\Exception $e) {
+            // we are even not able to render the error page, so we send the client a unicorn
+            $response = 'Page not found (' . $e->getMessage() . ') 🦄';
+        }
 
         $statusCode = 500;
         $headers = [];
@@ -121,80 +229,18 @@ class ResponseExceptionListener implements EventSubscriberInterface
             $headers = $exception->getHeaders();
         }
 
-        $host = $event->getRequest()->getHost();
+        $event->setResponse(new Response($response, $statusCode, $headers));
+    }
 
-        $pathInfo = array_values(array_filter(explode('/', $event->getRequest()->getPathInfo())));
-        $possibleLocaleSlug = $pathInfo[0];
-
-        //get host page.
-        $hostIndex = array_keys(array_filter($zoneDomains,
-            function ($v) use ($host, $possibleLocaleSlug) {
-                return $v['realHost'] === $host;
-            }));
-
-        //maybe there is a localized host page.
-        $languageIndex = false;
-        if (!empty($possibleLocaleSlug)) {
-            $languageIndex = array_keys(array_filter($zoneDomains,
-                function ($v) use ($host, $possibleLocaleSlug) {
-                    return $v['realHost'] === $host && $v['hrefLang'] === $possibleLocaleSlug;
-                }));
-        }
-
-        if ($languageIndex !== false) {
-            $hostIndex = $languageIndex[0];
-        } elseif ($hostIndex !== false) {
-            $hostIndex = $hostIndex[0];
-        }
-
-        $errorPath = null;
-
-        $defaultErrorPath = Config::getSystemConfig()->documents->error_pages->default;
-        if (Site::isSiteRequest()) {
-
-            $site = Site::getCurrentSite();
-            $siteErrorPath = $site->getErrorDocument();
-            if (!empty($siteErrorPath)) {
-                $defaultErrorPath = $siteErrorPath;
-            }
-        }
-
-        if ($hostIndex !== false) {
-
-            $guessedErrorPath = 'error';
-
-            //if we have a default error page, try to use same name.
-            if (!empty($defaultErrorPath)) {
-                $defaultErrorPathFragments = array_filter(explode('/', $defaultErrorPath));
-                if (!empty($defaultErrorPathFragments)) {
-                    $guessedErrorPath = end($defaultErrorPathFragments);
-                }
-            }
-
-            $path = $zoneDomains[$hostIndex]['fullPath'] . '/' . $guessedErrorPath;
-            if (Document\Service::pathExists($path)) {
-                $errorPath = $path;
-            };
-        }
-
-        //no custom error paths found, use system error path.
-        if (empty($errorPath)) {
-            $errorPath = $defaultErrorPath;
-        }
-
-        if (empty($errorPath)) {
-            $errorPath = '/';
-        }
-
-        $document = Document::getByPath($errorPath);
-
-        // default is home
-        if (!$document instanceof Document\Page) {
-            $document = Document::getById(1);
-        }
-
+    /**
+     * @param $documentLocale
+     */
+    private function setRuntime($documentLocale)
+    {
         //fix i18n language / country context.
-        $docLang = explode('_', $document->getProperty('language'));
+        Cache\Runtime::get('i18n.locale', $documentLocale);
+
+        $docLang = explode('_', $documentLocale);
         Cache\Runtime::set('i18n.languageIso', strtolower($docLang[0]));
 
         $countryIso = Definitions::INTERNATIONAL_COUNTRY_NAMESPACE;
@@ -204,14 +250,5 @@ class ResponseExceptionListener implements EventSubscriberInterface
         }
 
         Cache\Runtime::set('i18n.countryIso', $countryIso);
-
-        try {
-            $response = $this->actionRenderer->render($document);
-        } catch (\Exception $e) {
-            // we are even not able to render the error page, so we send the client a unicorn
-            $response = 'Page not found (' . $e->getLine() . ') 🦄';
-        }
-
-        $event->setResponse(new Response($response, $statusCode, $headers));
     }
 }
