99c290bc5a013efbd4e91c54932af261062f64a7
Stefan Hagspiel <shagspiel@dachcom.ch>
2019-08-21 17:52:05 +0200

    add not found test

--- tests/functional.zone.extended/PimcoreRedirectCest.php
+++ tests/functional.zone.extended/PimcoreRedirectCest.php
@@ -36,4 +36,36 @@ class PimcoreRedirectCest extends AbstractZone
         $I->amOnPageWithLocale('http://test-domain1.test/my-special-redirect', 'it');
         $I->seeCurrentUrlEquals('/it/riguardo-a-noi');
     }
+
+    /**
+     * @param FunctionalTester $I
+     */
+    public function testExtendedLocalizedRedirectPathNotFound(FunctionalTester $I)
+    {
+        $data = $this->setupSites($I);
+
+        $document1 = $I->haveASubPageDocument($data['document1'], 'about-us');
+        $document2 = $I->haveASubPageDocument($data['document2'], 'ueber-uns');
+        $document3 = $I->haveASubPageDocument($data['document3'], 'riguardo-a-noi');
+        $document4 = $I->haveASubPageDocument($data['site3']->getRootDocument(), 'propos-de-nous');
+
+        $I->haveTwoConnectedDocuments($document1, $document2);
+        $I->haveTwoConnectedDocuments($document1, $document3);
+        $I->haveTwoConnectedDocuments($document1, $document4);
+
+        $redirect = [
+            'type'       => 'path',
+            'source'     => '@^/my-special-redirect@',
+            'sourceSite' => null,
+            'target'     => sprintf('/{i18n_localized_target_page=%s}', $document3->getId()),
+            'targetSite' => null,
+            'statusCode' => 301,
+            'regex'      => 1,
+        ];
+
+        $I->haveAPimcoreRedirect($redirect);
+        $I->amOnPageWithLocale('http://test-domain1.test/my-special-redirect', 'it');
+        $I->seeCurrentUrlEquals('/my-special-redirect');
+        $I->canSeePageNotFound();
+    }
 }
