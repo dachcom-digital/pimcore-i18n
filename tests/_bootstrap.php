5782b691cdaae8f4f320233631bb049a15e624cb
Stefan Hagspiel <shagspiel@dachcom.ch>
2020-02-02 18:53:46 +0100

    fix notice errors

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -24,4 +24,8 @@ if (!isset($_SERVER['REQUEST_URI'])) {
     $_SERVER['REQUEST_URI'] = '';
 }
 
+if (!isset($_SERVER['HTTP_USER_AGENT'])) {
+    $_SERVER['HTTP_USER_AGENT'] = '';
+}
+
 define('PIMCORE_TEST', true);
