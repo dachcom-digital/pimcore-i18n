f84105fafe48075ec66ec4c6b2e5a7bfd30c1ac1
Stefan Hagspiel <shagspiel@dachcom.ch>
2020-02-21 22:00:00 +0100

    fix tests

--- tests/_support/Helper/PimcoreBackend.php
+++ tests/_support/Helper/PimcoreBackend.php
@@ -335,6 +335,10 @@ class PimcoreBackend extends Module
      */
     public function haveAUnPublishedDocument(Document $document)
     {
+        if (method_exists($document, 'setMissingRequiredEditable')) {
+            $document->setMissingRequiredEditable(false);
+        }
+
         $document->setPublished(false);
 
         try {
@@ -476,7 +480,6 @@ class PimcoreBackend extends Module
         return $hardlink;
     }
 
-
     /**
      * API Function to create a link document
      *
@@ -503,8 +506,6 @@ class PimcoreBackend extends Module
 
         return $link;
     }
-
-
 
     /**
      * API Function to create a site document
