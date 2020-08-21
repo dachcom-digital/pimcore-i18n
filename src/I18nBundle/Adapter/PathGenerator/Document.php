25f7d63e030ced1c7577c8c55f8c8775ad972aab
Stefan Hagspiel <shagspiel@dachcom.ch>
2020-08-21 10:06:20 +0200

    disable adding unavailable documents to zone tree (#66)

--- src/I18nBundle/Adapter/PathGenerator/Document.php
+++ src/I18nBundle/Adapter/PathGenerator/Document.php
@@ -148,6 +148,7 @@ class Document extends AbstractPathGenerator
         $rootDocumentId = array_search($currentDocument->getId(), array_column($tree, 'id'));
 
         if ($onlyShowRootLanguages === true || $rootDocumentId !== false) {
+
             foreach ($tree as $pageInfo) {
                 if (!empty($pageInfo['countryIso'])) {
                     $routes[] = [
@@ -161,100 +162,109 @@ class Document extends AbstractPathGenerator
                     ];
                 }
             }
-        } else {
-            $hardLinksToCheck = [];
-            $service = new PimcoreDocument\Service();
-            $translations = $service->getTranslations($currentDocument);
 
-            //if no translation has been found, add document itself:
-            if (empty($translations) && $currentDocument->hasProperty('language')) {
-                if ($currentDocument instanceof PimcoreDocument\Hardlink\Wrapper\WrapperInterface) {
-                    /** @var PimcoreDocument\Hardlink\Wrapper\WrapperInterface $wrapperDocument */
-                    $wrapperDocument = $currentDocument;
-                    $locale = $wrapperDocument->getHardLinkSource()->getSourceDocument()->getProperty('language');
-                } else {
-                    $locale = $currentDocument->getProperty('language');
-                }
+            return $routes;
+        }
 
-                $translations = [$locale => $currentDocument->getId()];
+        $hardLinksToCheck = [];
+        $service = new PimcoreDocument\Service();
+        $translations = $service->getTranslations($currentDocument);
+
+        //if no translation has been found, add document itself:
+        if (empty($translations) && $currentDocument->hasProperty('language')) {
+            if ($currentDocument instanceof PimcoreDocument\Hardlink\Wrapper\WrapperInterface) {
+                /** @var PimcoreDocument\Hardlink\Wrapper\WrapperInterface $wrapperDocument */
+                $wrapperDocument = $currentDocument;
+                $locale = $wrapperDocument->getHardLinkSource()->getSourceDocument()->getProperty('language');
+            } else {
+                $locale = $currentDocument->getProperty('language');
             }
 
-            foreach ($tree as $pageInfo) {
-                if (empty($pageInfo['locale'])) {
-                    continue;
-                }
+            $translations = [$locale => $currentDocument->getId()];
+        }
 
-                $pageInfoLocale = $pageInfo['locale'];
+        foreach ($tree as $pageInfo) {
 
-                if (isset($translations[$pageInfoLocale])) {
-                    try {
-                        /** @var PimcoreDocument\Page $document */
-                        $document = PimcoreDocument::getById($translations[$pageInfoLocale]);
-                    } catch (\Exception $e) {
-                        continue;
-                    }
+            if (empty($pageInfo['locale'])) {
+                continue;
+            }
 
-                    if (!$document->isPublished()) {
-                        continue;
-                    }
+            $pageInfoLocale = $pageInfo['locale'];
 
-                    $hasPrettyUrl = false;
-                    if ($this->hasPrettyUrl($document) === true) {
-                        $hasPrettyUrl = true;
-                        $relativePath = $document->getPrettyUrl();
-                        $url = System::joinPath([$pageInfo['domainUrl'], $relativePath]);
-                    } else {
-                        //map paths
-                        $documentPath = $document->getRealPath() . $document->getKey();
-                        $relativePath = preg_replace('/^' . preg_quote($pageInfo['fullPath'], '/') . '/', '', $documentPath);
-                        $url = System::joinPath([$pageInfo['url'], $relativePath]);
-                    }
-
-                    $routes[] = [
-                        'languageIso'      => $pageInfo['languageIso'],
-                        'countryIso'       => $pageInfo['countryIso'],
-                        'locale'           => $pageInfo['locale'],
-                        'hrefLang'         => $pageInfo['hrefLang'],
-                        'localeUrlMapping' => $pageInfo['localeUrlMapping'],
-                        'key'              => $document->getKey(),
-                        'relativePath'     => $relativePath,
-                        'hasPrettyUrl'     => $hasPrettyUrl,
-                        'url'              => $url
-                    ];
-
-                //document does not exist.
-                } else {
+            // document/translation does not exist.
+            // if page info is type of "hardlink", we need to add them to a second check
+            if (!isset($translations[$pageInfoLocale])) {
+                if ($pageInfo['type'] === 'hardlink') {
                     $hardLinksToCheck[] = $pageInfo;
                 }
+                continue;
             }
 
-            if (!empty($hardLinksToCheck)) {
-                foreach ($hardLinksToCheck as $hardLinkWrapper) {
-                    $sameLanguageContext = array_search($hardLinkWrapper['languageIso'], array_column($routes, 'languageIso'));
-                    if ($sameLanguageContext === false || !isset($routes[$sameLanguageContext])) {
-                        continue;
-                    }
-
-                    $languageContext = $routes[$sameLanguageContext];
-                    $posGlobalPath = System::joinPath([$hardLinkWrapper['fullPath'], $languageContext['relativePath']]);
-
-                    // case 1: only add hardlinks check if document has no pretty url => we can't guess pretty urls by magic.
-                    // case 2: always continue: could be disabled or isn't linked via translations.
-                    if ($languageContext['hasPrettyUrl'] === true || PimcoreDocument\Service::pathExists($posGlobalPath)) {
-                        continue;
-                    }
-
-                    $routes[] = [
-                        'languageIso'      => $hardLinkWrapper['languageIso'],
-                        'countryIso'       => $hardLinkWrapper['countryIso'],
-                        'locale'           => $hardLinkWrapper['locale'],
-                        'hrefLang'         => $hardLinkWrapper['hrefLang'],
-                        'localeUrlMapping' => $hardLinkWrapper['localeUrlMapping'],
-                        'key'              => $languageContext['key'],
-                        'url'              => System::joinPath([$hardLinkWrapper['url'], $languageContext['relativePath']])
-                    ];
-                }
+            try {
+                /** @var PimcoreDocument\Page $document */
+                $document = PimcoreDocument::getById($translations[$pageInfoLocale]);
+            } catch (\Exception $e) {
+                continue;
             }
+
+            if (!$document->isPublished()) {
+                continue;
+            }
+
+            $hasPrettyUrl = false;
+            if ($this->hasPrettyUrl($document) === true) {
+                $hasPrettyUrl = true;
+                $relativePath = $document->getPrettyUrl();
+                $url = System::joinPath([$pageInfo['domainUrl'], $relativePath]);
+            } else {
+                //map paths
+                $documentPath = $document->getRealPath() . $document->getKey();
+                $relativePath = preg_replace('/^' . preg_quote($pageInfo['fullPath'], '/') . '/', '', $documentPath);
+                $url = System::joinPath([$pageInfo['url'], $relativePath]);
+            }
+
+            $routes[] = [
+                'languageIso'      => $pageInfo['languageIso'],
+                'countryIso'       => $pageInfo['countryIso'],
+                'locale'           => $pageInfo['locale'],
+                'hrefLang'         => $pageInfo['hrefLang'],
+                'localeUrlMapping' => $pageInfo['localeUrlMapping'],
+                'key'              => $document->getKey(),
+                'relativePath'     => $relativePath,
+                'hasPrettyUrl'     => $hasPrettyUrl,
+                'url'              => $url
+            ];
+        }
+
+        if (empty($hardLinksToCheck)) {
+            return $routes;
+        }
+
+        foreach ($hardLinksToCheck as $hardLinkWrapper) {
+
+            $sameLanguageContext = array_search($hardLinkWrapper['languageIso'], array_column($routes, 'languageIso'));
+            if ($sameLanguageContext === false || !isset($routes[$sameLanguageContext])) {
+                continue;
+            }
+
+            $languageContext = $routes[$sameLanguageContext];
+            $posGlobalPath = System::joinPath([$hardLinkWrapper['fullPath'], $languageContext['relativePath']]);
+
+            // case 1: only add hardlinks check if document has no pretty url => we can't guess pretty urls by magic.
+            // case 2: always continue: could be disabled or isn't linked via translations.
+            if ($languageContext['hasPrettyUrl'] === true || PimcoreDocument\Service::pathExists($posGlobalPath)) {
+                continue;
+            }
+
+            $routes[] = [
+                'languageIso'      => $hardLinkWrapper['languageIso'],
+                'countryIso'       => $hardLinkWrapper['countryIso'],
+                'locale'           => $hardLinkWrapper['locale'],
+                'hrefLang'         => $hardLinkWrapper['hrefLang'],
+                'localeUrlMapping' => $hardLinkWrapper['localeUrlMapping'],
+                'key'              => $languageContext['key'],
+                'url'              => System::joinPath([$hardLinkWrapper['url'], $languageContext['relativePath']])
+            ];
         }
 
         return $routes;
